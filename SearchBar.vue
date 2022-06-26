fdda295163007c58a5433ac866c2fffd671de6b8
Nivfono2001 <81295283+Nivfono2001@users.noreply.github.com>
2022-06-26 13:34:03 +0300

    Add files via upload

---
+++ SearchBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <input type="text" v-model="search" placeholder="Search Recipes">
+  </div>
+</template>
+<script>
+
+export default{
+    data(){
+        return {
+            search:""
+        }    
+    }
+}
+</script>

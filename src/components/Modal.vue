95575866572302654cce733aeb6b5d4e55e0afc6
Nivfono2001 <81295283+Nivfono2001@users.noreply.github.com>
2022-06-28 22:08:56 +0300

    before pulling

---
+++ src/components/Modal.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-button @click="modalShow = !modalShow">Create New Recipe</b-button>
+
+    <b-modal v-model="modalShow">Hello From Modal!</b-modal>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        modalShow: false
+      }
+    }
+  }
+</script>
